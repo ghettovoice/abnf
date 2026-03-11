c1802111da97295c627a12d4cf543b527eb9aa3a
Vladimir Vershinin <ghettovoice@gmail.com>
2026-03-11 16:17:51 +0300

    Fix lint warnings

--- cmd/abnf/main.go
+++ cmd/abnf/main.go
@@ -268,7 +268,7 @@ func generateAction(_ context.Context, cmd *cli.Command) error {
 	if err != nil {
 		return cli.Exit(fmt.Errorf("open output file: %w", err), 1) //errtrace:skip
 	}
-	defer fd.Close()
+	defer fd.Close() //nolint:errcheck
 
 	if _, err := g.WriteTo(fd); err != nil {
 		return cli.Exit(fmt.Errorf("write generated code to file %s: %w", outPath, err), 1) //errtrace:skip
@@ -362,12 +362,12 @@ func generateAction2(_ context.Context, cmd *cli.Command) error {
 		}
 
 		if _, err = g.ReadFrom(fd); err != nil {
-			fd.Close()
+			fd.Close() //nolint:errcheck
 			errs = append(errs, fmt.Errorf("parse ABNF file %s: %w", in, err))
 			continue
 		}
 
-		fd.Close()
+		fd.Close() //nolint:errcheck
 
 		if cmd.Bool("verbose") {
 			fmt.Printf("ABNF file %s parsed\n", in)
